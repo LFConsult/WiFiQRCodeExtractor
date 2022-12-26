036d5a9308c6155db2c93752f43eb598f7115aa8
T3d <TeddyBear06@users.noreply.github.com>
2022-12-26 18:39:23 +0100

    Initial release version

---
+++ WindowsFormsApp1/WiFiQRCodeExtractor.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WiFiQRCodeExtractor
+{
+    partial class WiFiQRCodeExtractor
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.networkNameValue = new System.Windows.Forms.TextBox();
+            this.networkNameLabel = new System.Windows.Forms.Label();
+            this.previewCamera = new System.Windows.Forms.PictureBox();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.passwordCopiedLabel = new System.Windows.Forms.Label();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.rawQrcodeContentLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.previewCamera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // networkNameValue
+            // 
+            this.networkNameValue.Location = new System.Drawing.Point(150, 309);
+            this.networkNameValue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.networkNameValue.Name = "networkNameValue";
+            this.networkNameValue.Size = new System.Drawing.Size(379, 26);
+            this.networkNameValue.TabIndex = 0;
+            this.networkNameValue.Visible = false;
+            // 
+            // networkNameLabel
+            // 
+            this.networkNameLabel.AutoSize = true;
+            this.networkNameLabel.Location = new System.Drawing.Point(14, 314);
+            this.networkNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.networkNameLabel.Name = "networkNameLabel";
+            this.networkNameLabel.Size = new System.Drawing.Size(121, 20);
+            this.networkNameLabel.TabIndex = 1;
+            this.networkNameLabel.Text = "Nom du réseau ";
+            this.networkNameLabel.Visible = false;
+            // 
+            // previewCamera
+            // 
+            this.previewCamera.Location = new System.Drawing.Point(18, 18);
+            this.previewCamera.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.previewCamera.Name = "previewCamera";
+            this.previewCamera.Size = new System.Drawing.Size(518, 266);
+            this.previewCamera.TabIndex = 2;
+            this.previewCamera.TabStop = false;
+            // 
+            // restartButton
+            // 
+            this.restartButton.Location = new System.Drawing.Point(18, 369);
+            this.restartButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(300, 35);
+            this.restartButton.TabIndex = 3;
+            this.restartButton.Text = "Recommencer";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Visible = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // passwordCopiedLabel
+            // 
+            this.passwordCopiedLabel.AutoSize = true;
+            this.passwordCopiedLabel.Location = new System.Drawing.Point(126, 143);
+            this.passwordCopiedLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.passwordCopiedLabel.Name = "passwordCopiedLabel";
+            this.passwordCopiedLabel.Size = new System.Drawing.Size(311, 20);
+            this.passwordCopiedLabel.TabIndex = 4;
+            this.passwordCopiedLabel.Text = "Mot de passe copié dans le presse-papiers";
+            this.passwordCopiedLabel.Visible = false;
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Location = new System.Drawing.Point(117, 340);
+            this.instructionsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(298, 20);
+            this.instructionsLabel.TabIndex = 5;
+            this.instructionsLabel.Text = "Placez le QRCode WiFi devant la caméra";
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(423, 369);
+            this.exitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(112, 35);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Quitter";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // rawQrcodeContentLabel
+            // 
+            this.rawQrcodeContentLabel.AutoSize = true;
+            this.rawQrcodeContentLabel.Location = new System.Drawing.Point(14, 215);
+            this.rawQrcodeContentLabel.Name = "rawQrcodeContentLabel";
+            this.rawQrcodeContentLabel.Size = new System.Drawing.Size(171, 20);
+            this.rawQrcodeContentLabel.TabIndex = 7;
+            this.rawQrcodeContentLabel.Text = "RAW QRCode content";
+            this.rawQrcodeContentLabel.Visible = false;
+            // 
+            // WiFiQRCodeExtractor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 423);
+            this.ControlBox = false;
+            this.Controls.Add(this.rawQrcodeContentLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.passwordCopiedLabel);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.previewCamera);
+            this.Controls.Add(this.networkNameLabel);
+            this.Controls.Add(this.networkNameValue);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "WiFiQRCodeExtractor";
+            this.Text = "WiFiQRCodeExtractor";
+            ((System.ComponentModel.ISupportInitialize)(this.previewCamera)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox networkNameValue;
+        private System.Windows.Forms.Label networkNameLabel;
+        private System.Windows.Forms.PictureBox previewCamera;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.Label passwordCopiedLabel;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label rawQrcodeContentLabel;
+    }
+}
+
---
+++ WindowsFormsApp1/WiFiQRCodeExtractor.cs
@@ -0,0 +1,152 @@
+﻿using System.Text.RegularExpressions;
+using AForge.Video.DirectShow;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing;
+using AForge.Video;
+using System.Media;
+using System;
+using ZXing;
+
+namespace WiFiQRCodeExtractor
+{
+    /// <summary>
+    /// A simple program to extract content from WiFiQRCodes.
+    /// Just put the password into clipboard...
+    /// Thanks ZXing and AForge for the awesome work!
+    /// </summary>
+    public partial class WiFiQRCodeExtractor : Form
+    {
+        /// <summary>
+        /// The video capture device.
+        /// </summary>
+        public VideoCaptureDevice videoDevice;
+
+        /// <summary>
+        /// A frame.
+        /// </summary>
+        private Bitmap bitmap;
+
+        /// <summary>
+        /// Indicates if a frame containaing a QRCode was datected.
+        /// </summary>
+        public bool qrcodeDetected = false;
+
+        public WiFiQRCodeExtractor()
+        {
+            InitializeComponent();
+            startDetection();
+        }
+
+        /// <summary>
+        /// Grab the first video input source and start processing new frames.
+        /// </summary>
+        public void startDetection()
+        {
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoDevice = new VideoCaptureDevice(videoDevices[Convert.ToInt32(0)].MonikerString);
+            videoDevice.NewFrame += new NewFrameEventHandler(video_newFrame);
+            videoDevice.VideoResolution = videoDevice.VideoCapabilities[0];
+            videoDevice.Start();
+        }
+
+        /// <summary>
+        /// On every new frame from video capture source.
+        /// </summary>
+        public void video_newFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            bitmap = eventArgs.Frame;
+            IBarcodeReader reader = new BarcodeReader();
+            var result = reader.Decode(bitmap);
+
+            if (result != null)
+            {
+                if (qrcodeDetected == false)
+                {
+                    videoDevice.NewFrame -= new NewFrameEventHandler(video_newFrame);
+                    videoDevice.SignalToStop();
+                    previewCamera.Image = null;
+                    SystemSounds.Asterisk.Play();
+                    // Thanks https://gist.github.com/error454/3229018
+                    string ssidPattern = @"(?<=S:)((?:[^\;\?\""\$\[\\\]\+])|(?:\[\;,:]))+(?<!\;)(?=;)";
+                    string passwordPattern = @"(?<=P:)((?:\[\;,:])|(?:[^;]))+(?<!\;)(?=;)";
+                    string rawQrcodeContent = result.Text;
+                    Match ssidM = Regex.Match(rawQrcodeContent, ssidPattern);
+                    Match passwordM = Regex.Match(rawQrcodeContent, passwordPattern);
+                    // Not always in the same order
+                    // Ex. : WIFI:S:NetworkSSID;T:WPA;P:idjh876786;H:false;;
+                    // Ex. : WIFI:T:WPA;P:idjh876786;S:NetworkSSID;H:false;;
+                    Invoke((MethodInvoker)delegate {
+                        networkNameLabel.Visible = true;
+                        networkNameValue.Visible = true;
+                        networkNameValue.Text = ssidM.ToString();
+                        Clipboard.SetText(passwordM.ToString());
+                        restartButton.Visible = true;
+                        previewCamera.Visible = false;
+                        passwordCopiedLabel.Visible = true;
+                        instructionsLabel.Visible = false;
+                        #if DEBUG
+                        rawQrcodeContentLabel.Visible = true;
+                        rawQrcodeContentLabel.Text = result.Text;
+                        #endif
+                    });
+                }
+                qrcodeDetected = true;
+            }
+            else
+            {
+                try
+                {
+                    previewCamera.Image = bitmap;
+                    previewCamera.SizeMode = PictureBoxSizeMode.Zoom;
+                    Thread.Sleep(100);
+                }
+                catch (Exception ex)
+                {
+                    // Nothing to do here, just follow normal program execution.
+                }
+            }
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            videoDevice.NewFrame += new NewFrameEventHandler(video_newFrame);
+            qrcodeDetected = false;
+            startDetection();
+            restartButton.Visible = false;
+            passwordCopiedLabel.Visible= false;
+            previewCamera.Visible = true;
+            networkNameLabel.Visible = false;
+            networkNameValue.Visible = false;
+            instructionsLabel.Visible = true;
+            rawQrcodeContentLabel.Visible = false;
+            rawQrcodeContentLabel.Text = String.Empty;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            if (videoDevice.IsRunning)
+            {
+                videoDevice.NewFrame -= new NewFrameEventHandler(video_newFrame);
+
+                videoDevice.SignalToStop();
+
+                // wait ~ 3 seconds
+                for (int i = 0; i < 30; i++)
+                {
+                    if (!videoDevice.IsRunning)
+                        break;
+                    Thread.Sleep(100);
+                }
+
+                if (videoDevice.IsRunning)
+                {
+                    videoDevice.Stop();
+                }
+
+                videoDevice.Source = null;
+            }
+            this.Close();
+        }
+    }
+}
